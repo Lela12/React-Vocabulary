21c02e2baa94c6f12fc48c0188557ccda2a83c05
Lela12 <dbcocotn4@hanmail.net>
2021-12-10 12:14:31 +0900

    [Finish] react-router-dom

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,13 +1,16 @@
 import dummy from "../db/data.json";
+// url에 포함된 값을 얻을때
+import { useParams } from "react-router";
 
 export default function Day() {
   //dummy.words
-  const day = 1;
-  const wordList = dummy.words.filter((word) => word.day === day);
-  console.log(wordList);
+  //day가 특정값만 나타낼 수 있게
+  const { day } = useParams();
+  const wordList = dummy.words.filter((word) => word.day === Number(day));
 
   return (
     <>
+      <h2>Day {day}</h2>
       <table>
         <tbody>
           {wordList.map((word) => (

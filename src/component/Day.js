9e5c539ef69e7ed5293d0032b5c6aaf64b7c44ac
Lela12 <dbcocotn4@hanmail.net>
2021-12-10 15:51:14 +0900

    [Finish] custom hooks

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,24 +1,11 @@
 // url에 포함된 값을 얻을때
-import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import useFetch from "../hooks/useFetch";
 import Word from "./Word";
 
 export default function Day() {
-  //dummy.words
-  //day가 특정값만 나타낼 수 있게(주소창에 있는 문자열)
   const { day } = useParams();
-  // const wordList = dummy.words.filter((word) => word.day === Number(day));
-  const [words, setWords] = useState([]);
-
-  useEffect(() => {
-    fetch(`http://localhost:3004/words?day=${day}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setWords(data);
-      });
-  }, [day]);
+  const words = useFetch(`http://localhost:3004/words?day=${day}`);
 
   return (
     <>

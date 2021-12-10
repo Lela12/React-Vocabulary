a723beffceb1e327d4cd6f445f75d35bcadcfda3
Lela12 <dbcocotn4@hanmail.net>
2021-12-10 15:40:34 +0900

    [Finish] useEffect, fetch()

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ function App() {
       <div className="App">
         <Header />
         <Switch>
+          {/* 첫번째 페이지를 의미 */}
           <Route exact path="/">
             <DayList />
           </Route>
+          {/* url에 있는 day 1의 값을 얻고 싶을때 */}
           <Route path="/day/:day">
             <Day />
           </Route>
--- src/component/Day.js
+++ src/component/Day.js
@@ -1,23 +1,32 @@
-import dummy from "../db/data.json";
 // url에 포함된 값을 얻을때
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import Word from "./Word";
 
 export default function Day() {
   //dummy.words
-  //day가 특정값만 나타낼 수 있게
+  //day가 특정값만 나타낼 수 있게(주소창에 있는 문자열)
   const { day } = useParams();
-  const wordList = dummy.words.filter((word) => word.day === Number(day));
+  // const wordList = dummy.words.filter((word) => word.day === Number(day));
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3004/words?day=${day}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setWords(data);
+      });
+  }, [day]);
 
   return (
     <>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map((word) => (
-            <tr key={word.id}>
-              <td>{word.eng}</td>
-              <td>{word.kor}</td>
-            </tr>
+          {words.map((word) => (
+            <Word word={word} key={word.id} />
           ))}
         </tbody>
       </table>
